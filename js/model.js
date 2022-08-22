d346855c8d64fa48220bd1fe43e3440abaed41c9
Mohammed BarBar <mohammedking71@gmail.com>
2022-08-22 16:12:54 +0300

    Make some Changes

--- js/model.js
+++ js/model.js
@@ -3,12 +3,16 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    checkbox: document.querySelector("[check-box]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  refs.checkbox.addEventListener("click", toggleModal1);
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+  }
+  function toggleModal1() {
+    refs.modal.classList.toggle("is-checked");
   }
 })();
